1b4b7b510a0e96691a6f2ed7ab51b8c5fa0ebf84
GET Academy <terje@kolderup.net>
2026-06-11 09:58:19 +0200

    x

--- ReallySimpleApiWithFrontend/Program.cs
+++ ReallySimpleApiWithFrontend/Program.cs
@@ -17,28 +17,3 @@ app.MapPost("/api/contact", async (Contact contact) =>
 
 app.UseStaticFiles();
 app.Run();
-
-
-/*
- Synkron versjon
-
-using ReallySimpleApiWithFrontend;
-   
-   var builder = WebApplication.CreateBuilder(args);
-   var app = builder.Build();
-   app.UseHttpsRedirection();
-   
-   app.MapGet("/api/contact", () =>
-   {
-       Console.WriteLine(DateTime.Now + " - Leste alle kontaktene");
-       return ContactsRepository.Get();
-   });
-   app.MapPost("/api/contact", (Contact contact) =>
-   {
-       Console.WriteLine(DateTime.Now + $" - La til kontakten {contact.Name}");
-       ContactsRepository.Add(contact);
-   });
-   
-   app.UseStaticFiles();
-   app.Run();
-*/
