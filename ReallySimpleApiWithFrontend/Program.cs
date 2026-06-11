7a82af87c070e888a0b10575c2b91476c67d7b34
GET Academy <terje@kolderup.net>
2026-06-11 10:39:49 +0200

    Update Program.cs

--- ReallySimpleApiWithFrontend/Program.cs
+++ ReallySimpleApiWithFrontend/Program.cs
@@ -14,13 +14,16 @@ app.MapGet("/api/contact", async () =>
     Console.WriteLine(DateTime.Now + " - Leste alle kontaktene");
     var conn = new SqlConnection(connStr);
     var sql = "SELECT * FROM Contacts";
-    var contacts = await conn.QueryAsync(sql);
+    var contacts = await conn.QueryAsync<Contact>(sql);
     return contacts;
     //return await ContactsRepository.GetAsync();
 });
 app.MapPost("/api/contact", async (Contact contact) =>
 {
     Console.WriteLine(DateTime.Now + $" - La til kontakten {contact.Name}");
+    var conn = new SqlConnection(connStr);
+    var sql = "INSERT INTO Contacts VALUES (@Name, @Email)";
+    await conn.ExecuteAsync(sql, contact);
     //await ContactsRepository.AddAsync(contact);
 });
 
