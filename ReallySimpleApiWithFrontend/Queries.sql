6f36d16b54e7d2a386d17d918ff075da4dd5d5c6
GET Academy <terje@kolderup.net>
2026-06-11 10:22:24 +0200

    x

---
+++ ReallySimpleApiWithFrontend/Queries.sql
@@ -0,0 +1,22 @@
+﻿
+-- Create
+INSERT INTO Contacts
+VALUES ('x', 'x@gmail.com')
+
+-- Read
+SELECT *
+FROM Contacts
+
+SELECT *
+FROM Contacts
+WHERE Name = 'Terje'
+
+-- Update
+UPDATE Contacts
+SET Email = 'pal@gmail.com'
+WHERE Name = 'Pål'
+
+
+-- Delete
+DELETE FROM Contacts
+WHERE Name = 'x'
